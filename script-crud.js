dbc1cd6f0caf1644d74d205a1ba5334a74656a32
Reverton Silva <revertong@gmail.com>
2023-11-26 14:57:14 -0700

    Refazendo o JS comentado

--- script-crud.js
+++ script-crud.js
@@ -1,207 +1,37 @@
-const taskListContainer = document.querySelector('.app__section-task-list')
+// Aqui começamos por selecionar os elementos que vamos precisar interagir no nosso código.
+// Esta linha pega o botão de adicionar tarefa baseado na classe CSS.
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 
-const formTask = document.querySelector('.app__form-add-task')
-const toggleFormTaskBtn = document.querySelector('.app__button--add-task')
-const formLabel = document.querySelector('.app__form-label')
+// Da mesma forma, esta linha seleciona nosso formulário de adicionar tarefa.
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 
-const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel')
-
-const taskAtiveDescription = document.querySelector('.app__section-active-task-description')
-
+// E aqui, pegamos a área de texto onde o usuário digita a descrição da tarefa.
 const textarea = document.querySelector('.app__form-textarea')
 
-const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
+// Esta é a nossa lista (ou array) de tarefas. Ela começa vazia porque ainda não adicionamos nenhuma tarefa.
+const tarefas = []
 
-const btnDeletar = document.querySelector('.app__form-footer__button--delete')
-
-const btnDeletarConcluidas = document.querySelector('#btn-remover-concluidas')
-const btnDeletarTodas = document.querySelector('#btn-remover-todas')
-
-const localStorageTarefas = localStorage.getItem('tarefas')
-let tarefas = localStorageTarefas ? JSON.parse(localStorageTarefas) : []
-
-const taskIconSvg = `
-<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
-    fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="12" fill="#FFF" />
-    <path
-        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
-        fill="#01080E" />
-</svg>
-`
-
-let tarefaSelecionada = null
-let itemTarefaSelecionada = null
-
-let tarefaEmEdicao = null
-let paragraphEmEdicao = null
-
-const removerTarefas = (somenteConcluidas) => {
-    const seletor = somenteConcluidas ? '.app__section-task-list-item-complete' : '.app__section-task-list-item'
-    document.querySelectorAll(seletor).forEach((element) => {
-        element.remove();
-    });
-
-    tarefas = somenteConcluidas ? tarefas.filter(t => !t.concluida) : []
-    updateLocalStorage()
-}
-
-const selecionaTarefa = (tarefa, elemento) => {
-    if (tarefa.concluida) {
-        return
-    }
-
-    document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button) {
-        button.classList.remove('app__section-task-list-item-active')
-    })
-
-    if (tarefaSelecionada == tarefa) {
-        taskAtiveDescription.textContent = null
-        itemTarefaSelecionada = null
-        tarefaSelecionada = null
-        return
-    }
-
-    tarefaSelecionada = tarefa
-    itemTarefaSelecionada = elemento
-    taskAtiveDescription.textContent = tarefa.descricao
-    elemento.classList.add('app__section-task-list-item-active')
-}
-
-const limparForm = () => {
-    tarefaEmEdicao = null
-    paragraphEmEdicao = null
-    textarea.value = ''
-    formTask.classList.add('hidden')
-}
-
-const selecionaTarefaParaEditar = (tarefa, elemento) => {
-    if(tarefaEmEdicao == tarefa) {
-        limparForm()
-        return
-    }
-
-    formLabel.textContent='Editando tarefa'
-    tarefaEmEdicao=tarefa
-    paragraphEmEdicao=elemento
-    textarea.value = tarefa.descricao
-    formTask.classList.remove('hidden')
-}
-
-function createTask(tarefa) {
-    const li = document.createElement('li')
-    li.classList.add('app__section-task-list-item')
-
-    const svgIcon = document.createElement('svg')
-    svgIcon.innerHTML = taskIconSvg
-
-    const paragraph = document.createElement('p')
-    paragraph.classList.add('app__section-task-list-item-description')
-
-    paragraph.textContent = tarefa.descricao
-
-    const button = document.createElement('button')
-
-    button.classList.add('app_button-edit')
-    const editIcon = document.createElement('img')
-    editIcon.setAttribute('src', '/imagens/edit.png')
-
-    button.appendChild(editIcon)
-
-    button.addEventListener('click', (event) =>{
-        event.stopPropagation()
-        selecionaTarefaParaEditar(tarefa, paragraph)
-    })
-
-    li.onclick = () => {
-        selecionaTarefa(tarefa, li)
-    }
-
-    svgIcon.addEventListener('click', (event) => {
-        if (tarefa == tarefaSelecionada) {
-            event.stopPropagation()
-            button.setAttribute('disabled', true)
-            li.classList.add('app__section-task-list-item-complete')
-            tarefaSelecionada.concluida = true
-            updateLocalStorage()
-        }
-    })
-
-    if(tarefa.concluida){
-        button.setAttribute('disabled', true)
-        li.classList.add('app__section-task-list-item-complete')
-    }
-
-    li.appendChild(svgIcon)
-    li.appendChild(paragraph)
-    li.appendChild(button)
-    
-    return li
-}
-
-tarefas.forEach(task => {
-    const taskItem = createTask(task)
-    taskListContainer.appendChild(taskItem)
+// Agora, adicionamos um ouvinte de eventos ao botão. Quando o botão for clicado, esta função será executada.
+btnAdicionarTarefa.addEventListener('click', () => {
+    // Esta linha vai alternar a visibilidade do nosso formulário. Lembra da classe 'hidden' que esconde elementos?
+    formAdicionarTarefa.classList.toggle('hidden')
 })
 
-cancelFormTaskBtn.addEventListener('click', () => {
-    formTask.classList.add('hidden')
-})
+// Aqui, estamos ouvindo o evento de 'submit' do nosso formulário. 
+// Esse evento ocorre quando tentamos enviar o formulário (geralmente, apertando o botão 'Enter' ou clicando em um botão de submit).
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    // Esta linha evita que a página recarregue (comportamento padrão de um formulário). Nós não queremos isso!
+    evento.preventDefault();
 
-btnCancelar.addEventListener('click', limparForm)
-
-toggleFormTaskBtn.addEventListener('click', () => {
-    formLabel.textContent = 'Adicionando tarefa'
-    formTask.classList.toggle('hidden')
-})
-
-btnDeletar.addEventListener('click', () => {
-    if (tarefaSelecionada) {
-        const index = tarefas.indexOf(tarefaSelecionada);
-        if (index !== -1) {
-            tarefas.splice(index, 1);
-        }
-        
-        itemTarefaSelecionada.remove()
-        tarefas.filter(t => t != tarefaSelecionada) 
-        itemTarefaSelecionada = null
-        tarefaSelecionada = null
+    // Aqui, criamos um objeto tarefa com a descrição vinda da nossa textarea.
+    const tarefa = {
+        descricao: textarea.value
     }
 
-    updateLocalStorage()
-    limparForm()
-})
+    // Depois, adicionamos essa tarefa ao nosso array de tarefas.
+    tarefas.push(tarefa)
 
-const updateLocalStorage = () => {
+    // E, finalmente, armazenamos nossa lista de tarefas no localStorage. 
+    // Convertendo o array para uma string em formato JSON para poder armazenar.
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
-}
-
-formTask.addEventListener('submit', (evento) => {
-    evento.preventDefault()
-    if(tarefaEmEdicao) {
-        tarefaEmEdicao.descricao = textarea.value
-        paragraphEmEdicao.textContent = textarea.value
-    } else {
-    const task = {
-        descricao: textarea.value,
-        concluida: false
-    }
-    tarefas.push(task)
-    const taskItem = createTask(task)
-    taskListContainer.appendChild(taskItem)
-}
-    updateLocalStorage()
-    limparForm()
 })
-
-btnDeletarConcluidas.addEventListener('click', () => removerTarefas(true))
-btnDeletarTodas.addEventListener('click', () => removerTarefas(false))
-
-document.addEventListener("TarefaFinalizada", function (e) {
-    if (tarefaSelecionada) {
-        tarefaSelecionada.concluida = true
-        itemTarefaSelecionada.classList.add('app__section-task-list-item-complete')
-        itemTarefaSelecionada.querySelector('button').setAttribute('disabled', true)
-        updateLocalStorage()
-    }
-});
